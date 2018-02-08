54229b787a3983961ce5ad64478428dd96665303
G. Zentkovich <gzentkovich@gmail.com>
2018-02-08 00:12:57 -1000

    modified index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ Aloha: 'Friends' });
 });
 
 const PORT = process.env.PORT || 5000;
